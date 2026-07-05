ba7c1be0b62c676f34eac3c474143fa257fde37e
taeu <btu0414@gmail.com>
2026-07-05 16:27:19 +0900

    chore: dedupe_identical_frontend_files

--- TrustChain/src/components/BottomControlBar.jsx
+++
@@ -1,2 +0,0 @@
-import React from "react";
-export default function BottomControlBar({onOpen}){return <div className="bottom-control-bar" onClick={onOpen}>분석 보고서 ▲</div>}
--- TrustChain/src/components/DetectionCard.jsx
+++
@@ -1,2 +0,0 @@
-import React from "react";
-export default function DetectionCard({title,desc,badge}){return <div className="bio-row"><div><div>{title}</div><div>{desc}</div></div><span>{badge}</span></div>}
